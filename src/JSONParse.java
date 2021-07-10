0f709751c71b698f8b8b16971d5908568e07ecc0
Emily Wang <ewang56@uwo.ca>
2021-07-10 14:06:38 -0600

    deleted plot

--- src/JSONParse.java
+++ src/JSONParse.java
@@ -1,38 +1,38 @@
-import java.io.IOException;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+// import java.io.IOException;
+// import com.google.gson.JsonElement;
+// import com.google.gson.JsonObject;
+// import com.google.gson.JsonParser;
 
-public class JSONParse {
+// public class JSONParse {
 
-	private BitcoinData bitcoinData;
+// 	private BitcoinData bitcoinData;
 	
-	public JSONParse() throws IOException {
-		CoindeskAPI current = new CoindeskAPI();
-		String json = current.getBitcoinData();
+// 	public JSONParse() throws IOException {
+// 		CoindeskAPI current = new CoindeskAPI();
+// 		String json = current.getBitcoinData();
 		
-		JsonElement element0 = JsonParser.parseString(json);
-		System.out.println(element0.toString());
+// 		JsonElement element0 = JsonParser.parseString(json);
+// 		System.out.println(element0.toString());
 
-		JsonElement time = ((JsonObject) element0).get("time");
-		JsonElement bpi = ((JsonObject) element0).get("bpi");
+// 		JsonElement time = ((JsonObject) element0).get("time");
+// 		JsonElement bpi = ((JsonObject) element0).get("bpi");
 		
-		JsonElement element1 = JsonParser.parseString(time.toString());
-		JsonElement element2 = JsonParser.parseString(bpi.toString());
+// 		JsonElement element1 = JsonParser.parseString(time.toString());
+// 		JsonElement element2 = JsonParser.parseString(bpi.toString());
 		
-		JsonElement updatedTime = ((JsonObject) element1).get("updated"); 
-		JsonElement USD = ((JsonObject) element2).get("USD"); 
+// 		JsonElement updatedTime = ((JsonObject) element1).get("updated"); 
+// 		JsonElement USD = ((JsonObject) element2).get("USD"); 
 		
-		JsonElement element3 = JsonParser.parseString(USD.toString());
-		JsonElement rate_float = ((JsonObject) element3).get("rate_float");
+// 		JsonElement element3 = JsonParser.parseString(USD.toString());
+// 		JsonElement rate_float = ((JsonObject) element3).get("rate_float");
 
-		Double rateUSD = Double.parseDouble(rate_float.toString());
+// 		Double rateUSD = Double.parseDouble(rate_float.toString());
 
-		bitcoinData = new BitcoinData(updatedTime.toString(), rateUSD);
-	}
+// 		bitcoinData = new BitcoinData(updatedTime.toString(), rateUSD);
+// 	}
 	
-	public BitcoinData getBitcoinData() {
-		return bitcoinData;
-	}
+// 	public BitcoinData getBitcoinData() {
+// 		return bitcoinData;
+// 	}
 
-}
+// }
