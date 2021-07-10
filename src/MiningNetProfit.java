1e2be727744ca7620c91c8dbd771d85c60815844
GraydonHall42 <gwhall@ualberta.ca>
2021-07-10 12:46:06 -0600

    added logic to calculate hourly profit of a mining rig

--- src/MiningNetProfit.java
+++ src/MiningNetProfit.java
@@ -2,16 +2,47 @@ import java.util.Arrays;
 
 public class MiningNetProfit {
     private double btcRate;  // price for BTC in USD
+
     private double minerWattage;  // watts this miner uses
     private double minerRate;  // rate of BTC/hr
     private double[] elecCost;  // 1D array of the electricity costs each hour (cents/KWh)
 
+    public MiningNetProfit(double btcRate, double minerWattage, double minerRate, double[] elecCost) {
+        this.btcRate = btcRate;
+        this.minerWattage = minerWattage;
+        this.minerRate = minerRate;
+        this.elecCost = elecCost;
+    }
+
+    public double[] getNetProfitHourly() {
+        // net profit (for that hour)= btc rate ($/btc) * miner rate (BTC/hr) - wattage (W)/1000 * (cents/kWh) / 100
+        double hourlyProfit;
+        netProfitHourly = new double[elecCost.length];
+
+        for(int i=0; i<elecCost.length; i++){
+            hourlyProfit = btcRate * minerRate - minerWattage/1000*elecCost[i]/100;
+            netProfitHourly[i] = hourlyProfit;
+        }
+
+        return netProfitHourly;
+    }
+
+    private double[] netProfitHourly;
+
+
+
     // idea:
-    // net profit (for that hour)= btc rate ($/btc) * miner rate (BTC/hr) - wattage (W)/1000 * (cents/kWh) * 100
+
 
     public static void main(String[] args) {
-        var elecCostTest = new int[{6, 6, 7, 7, 6, 6, 7, 7, 6, 6, 7, 7, 6, 6, 7, 7, 6, 6, 7, 7, 6, 6, 7, 7};
-        System.out.println(Arrays.toString(elecCostTest));
+        double[] elecCostTest  = {6, 6, 7, 7, 6, 6, 7, 7, 6, 6, 7, 7, 6, 6, 7, 7, 6, 6, 7, 7, 6, 6, 7, 7};
+        double btcRateTest = 33694.9726;
+        double minerWattageTest = 823;
+        double minerRateTest = 0.00010294;
+
+        var x = new MiningNetProfit(btcRateTest, minerWattageTest, minerRateTest, elecCostTest);
+
+        System.out.println(Arrays.toString(x.getNetProfitHourly()));
     }
 
 
