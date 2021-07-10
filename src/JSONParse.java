88990072564e857b44a52820b9981f3a368e9c91
Brandon Quan <30513301+bquan057@users.noreply.github.com>
2021-07-10 13:41:06 -0600

    Added JSONParse and BitcoinData classes to parse through info from API and store it.

---
+++ src/JSONParse.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class JSONParse {
+
+	private BitcoinData bitcoinData;
+	
+	public JSONParse() throws IOException {
+		CoindeskAPI current = new CoindeskAPI();
+		String json = current.getBitcoinData();
+		
+		JsonElement element0 = JsonParser.parseString(json);
+		System.out.println(element0.toString());
+
+		JsonElement time = ((JsonObject) element0).get("time");
+		JsonElement bpi = ((JsonObject) element0).get("bpi");
+		
+		JsonElement element1 = JsonParser.parseString(time.toString());
+		JsonElement element2 = JsonParser.parseString(bpi.toString());
+		
+		JsonElement updatedTime = ((JsonObject) element1).get("updated"); 
+		JsonElement USD = ((JsonObject) element2).get("USD"); 
+		
+		JsonElement element3 = JsonParser.parseString(USD.toString());
+		JsonElement rate_float = ((JsonObject) element3).get("rate_float");
+
+		Double rateUSD = Double.parseDouble(rate_float.toString());
+
+		bitcoinData = new BitcoinData(updatedTime.toString(), rateUSD);
+	}
+	
+	public BitcoinData getBitcoinData() {
+		return bitcoinData;
+	}
+
+}
